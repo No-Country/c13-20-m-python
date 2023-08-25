fec829f603855650f625bfe1e914358e64d4dfdb
Juan Cruz Carrizo <juancarrizoast@gmail.com>
2023-08-25 11:43:00 -0300

    adding split allowed hosts

--- servidor/src/mesumo/settings/production.py
+++ servidor/src/mesumo/settings/production.py
@@ -1,11 +1,11 @@
 from .base import *
 import dj_database_url
 
-DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
+DEBUG = os.environ.get("DEBUG")
 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS")
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
 DATABASES = {
 
