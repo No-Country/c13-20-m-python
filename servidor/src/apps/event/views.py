a92f09df0204e844f11391dc0b1b688f75e18ec8
JuanBianchi <juampibianchi065@gmail.com>
2023-08-26 15:35:44 -0300

    working on event image upload

--- servidor/src/apps/event/views.py
+++ servidor/src/apps/event/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .models import Event
+from .serializer import EventSerializers
 
 # Create your views here.
+
+class EventViewSet(ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializers
