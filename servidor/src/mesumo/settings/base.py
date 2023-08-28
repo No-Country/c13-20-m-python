45c8d02311a31a1f52b401993238723305409285
LucasSimoesMDP <lucasgsimoes@icloud.com>
2023-08-28 13:24:03 -0300

    base.py modified (cors issue)

--- servidor/src/mesumo/settings/base.py
+++ servidor/src/mesumo/settings/base.py
@@ -14,7 +14,9 @@ DJANGO_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'rest_framework',
+
 ]
 
 
@@ -37,6 +39,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'mesumo.urls'
