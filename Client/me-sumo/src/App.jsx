447f7d19fa96018077766672e748708bb762949e
sofiacostamagna <sofiacostamagna45@gmail.com>
2023-08-29 10:53:39 -0300

    Interests

--- Client/me-sumo/src/App.jsx
+++ Client/me-sumo/src/App.jsx
@@ -2,7 +2,6 @@ import "./App.css";
 import { Provider } from "react-redux";
 import { store } from "../src/redux/store";
 import { Route, Routes } from "react-router-dom";
-import LoginForm from "./components/LoginForm";
 import SignUpForm from "./components/SignUpForm";
 import Landing from "./views/Landing";
 import Interests from "./views/Interests";
