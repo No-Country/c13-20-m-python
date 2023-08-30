7fdcba8e9861c547cae5d291ba54f730a8ec0931
jramire5 <jjnicoramirez@gmail.com>
2023-08-30 18:39:13 -0300

    front/create-event: Creacion de componente Checks

--- Client/me-sumo/src/views/CreateEvent.jsx
+++ Client/me-sumo/src/views/CreateEvent.jsx
@@ -1,7 +1,7 @@
 import {useForm} from "react-hook-form"
 import Input from "../components/Shared/Input";
-import check_circle from "../assets/icons/check_circle.svg"
 import { Button } from "@material-tailwind/react";
+import Checks from "../components/Shared/checks";
 // import construction from "../assets/under-construction.png";
 
 
@@ -19,36 +19,12 @@ export default function CreateEvent() {
                   Crear un nuevo Evento
             </h1>
           </div>
-
+          <Checks/>
           <div className="flex justify-center w-full h-auto ">
-            <div className="w-full max-w-screen-md xl:max-w-screen-lg p-4 h-screen lg:py-0">
-          <div className="flex justify-center items-center flex-row p-4">
-            <div className="">
-              <img
-                src={check_circle}
-                className="h-auto m-1"
-              />
-              <p className="text-left">Informacion <br/> Basica</p>
-            </div>
-            <div className="">. . . . . . .</div>
-            <div className="">
-              <img
-                src={check_circle}
-                className="h-auto m-1"
-              />
-              <p className="text-left">Entradas <br /> </p>
-            </div >
-            <div className=""><p>. . . . . . .</p></div>
-            <div className="">
-              <img
-                src={check_circle}
-                className="h-auto  m-1"
-              />
-              <p className="text-left">Publicar <br /></p>
-            </div>
-          </div>
+            <div className="w-full max-w-screen-md xl:max-w-screen-lg p-4 lg:py-0">
+
               <form
-                className="space-y-1 w-full"
+                className="space-y-1 w-full mb-4"
                 action="#"
                 onSubmit={handleSubmit(() => {
                   //handleRegister(data);
@@ -63,6 +39,8 @@ export default function CreateEvent() {
                     register={register}
 //                    error={errors.email?.message}
                   />
+                  <label className="m-0 p-0 flex mb-2 ml-1 text-sm font-small text-gray-600 dark:text-white">Escriba el nombre del evento</label>
+
                 </div>
                 <div>
                   <Input
@@ -73,10 +51,11 @@ export default function CreateEvent() {
                     register={register}
 //                    error={errors.email?.message}
                   />
+                  <label className="m-0 p-0 flex mb-2 ml-1 text-sm font-small text-gray-600 dark:text-white">Añada una breve descripción</label>
                 </div>
                 <div>
                   <label           className="flex mb-2 ml-1 text-sm font-medium text-gray-900 dark:text-white">Categoría</label>
-                <select
+          <select
           required={true}
           { ...register('categoria') }
           className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
@@ -84,9 +63,11 @@ export default function CreateEvent() {
             {categorias.map((cat, index) => (
               <option className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
               key={index} value={cat}>{cat}</option>
-            ))}
+              ))}
           </select>
-                </div>
+          <label           className="flex mb-2 ml-1 text-sm font-small text-gray-600 dark:text-white">Elija una categoria</label>
+
+                     </div>
 <div className="flex flex-col md:flex-row w-full">
   <div className="md:w-1/2 md:pr-2">
     <Input
@@ -107,7 +88,7 @@ export default function CreateEvent() {
     />
   </div>
 </div>
-<div>
+<div className="mb-3">
                   <Input
                     labelText="Imagen de Portada"
                     type="file"
@@ -117,10 +98,10 @@ export default function CreateEvent() {
 //                    error={errors.email?.message}
                   />
                 </div>
-                <div className="bg-gray-50 absolute bottom-0 left-0 right-0 flex justify-center p-4">
-  <Button variant="outlined" className="m-2" title="Cancelar">Cancelar</Button>
-  <Button title="Crear Evento" className="m-2">Continuar → </Button>
-</div>
+                <div className="bg-gray-50 sticky bottom-0 left-0 right-0 flex justify-center p-4">
+                  <Button variant="outlined" className="m-2" title="Cancelar">Cancelar</Button>
+                  <Button title="Crear Evento" className="m-2">Continuar → </Button>
+                </div>
               </form>
             </div>
           </div>
