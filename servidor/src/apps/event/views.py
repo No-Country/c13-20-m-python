ae4966dee6390030cf13b2db5bb4fbd53a18f88e
LucasSimoesMDP <lucasgsimoes@icloud.com>
2023-08-31 14:08:53 -0300

    filters added:date,location,host_username | requirements.txt updated

--- servidor/src/apps/event/views.py
+++ servidor/src/apps/event/views.py
@@ -1,13 +1,15 @@
 from .models import Event
-from rest_framework import permissions,response, views, status
+from rest_framework import permissions,response, views, status, filters, generics
 from .serializer import EventSerializer, EventDetailSerializer
 from apps.user import authentication
-from rest_framework.decorators import api_view
-from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
+
 
 class EventView(views.APIView):
     authentication_classes = (authentication.CustomUserAuthentication, )
     permission_classes = (permissions.IsAuthenticated, ) 
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filterset_fields  = ('location','date','eventHost__username')
 
     def post(self,request):
         serializer = EventSerializer(data=request.data, context={'request': request})
@@ -19,13 +21,24 @@ class EventView(views.APIView):
             }, status=status.HTTP_201_CREATED)
         else:
             return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        # filter_backends = (SearchFilter, OrderingFilter)
-        # search_fields = ('location','date','eventHost__username')
     
-    def get(self,request):
+    def get(self, request):
         event = Event.objects.all()
-        serializer = EventSerializer(event, many= True)
-        return response.Response(serializer.data)
+        
+        location = request.query_params.get('location')
+        if location:
+            event = event.filter(location__icontains=location)
+        
+        date = request.query_params.get('date')
+        if date:
+            event = event.filter(date=date)
+        
+        eventHost_username = request.query_params.get('eventHost__username')
+        if eventHost_username:
+            event = event.filter(eventHost__username=eventHost_username)
+        
+        serializer = EventSerializer(event, many=True)
+        return response.Response(serializer.data)    
 
 class EventDetailView(views.APIView):    
     def get(self, request, pk):        
@@ -35,4 +48,4 @@ class EventDetailView(views.APIView):
             return response.Response({"detail": "Event not found"}, status=status.HTTP_404_NOT_FOUND)
 
         serializer = EventDetailSerializer(event)
-        return response.Response(serializer.data)
+        return response.Response(serializer.data)   
