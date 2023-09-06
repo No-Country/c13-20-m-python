4aca856497c51f1396377d30a0597b404db0b95b
Rodri <r.fontan12@gmail.com>
2023-09-06 12:03:44 -0300

    add map to create event

--- Client/me-sumo/src/redux/store.js
+++ Client/me-sumo/src/redux/store.js
@@ -2,11 +2,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import userHandler from "./sliceLogin";
 import eventsHandler from "./sliceEvents";
 import interestsHandler from "./sliceInterests";
+import createEventHandler from "./sliceCreateEvent";
 
 export const store = configureStore({
   reducer: {
     user: userHandler,
     events: eventsHandler,
     interests: interestsHandler,
+    createEvent: createEventHandler,
   },
 });
