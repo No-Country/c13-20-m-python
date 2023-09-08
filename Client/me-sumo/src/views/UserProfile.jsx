d22f8dfb7a2e0d2539883a1a3d91509b18b6e6e0
Rodri <r.fontan12@gmail.com>
2023-09-08 12:28:22 -0300

    basic user profile template

--- Client/me-sumo/src/views/UserProfile.jsx
+++ Client/me-sumo/src/views/UserProfile.jsx
@@ -12,7 +12,7 @@ export default function UserProfile() {
     <div className='bg-gray-100 min-h-screen p-4'>
       <div className='max-w-2xl flex mx-auto bg-white rounded-lg shadow-lg p-6'>
         <div className='bg-gray-800 text-white w-1/4 relative top-0 left-0 flex-col'>
-          <div className='p-6 text-2xl font-semibold'>Menú</div>
+          <div className='p-6 my-2 text-2xl font-semibold'>Menú</div>
           <ul className='flex flex-col p-4'>
             <li className='my-3'>
               <Link to='/mis-datos' className='hover:bg-gray-600 p-2 rounded'>
@@ -33,7 +33,7 @@ export default function UserProfile() {
         </div>
         <div className='p-8'>
           <div className='flex justify-center items-center mb-4'>
-            <img src='' alt='Foto de perfil' className='w-16 h-16 rounded-full mr-4' />
+            {/* <img src='' alt='Foto de perfil' className='w-16 h-16 rounded-full mr-4' /> */}
             <h1 className='text-2xl font-semibold mb-4'>Perfil de Usuario</h1>
           </div>
           <div>
